1c2a063476b2879a93820fca2719ba346e27326c
brockv <vancebrock22@gmail.com>
2020-06-01 20:18:37 -0700

    The wrong script was attached to the SuccessMenu scene buttons. Corrected that.

---
+++ Assets/Scripts/Menus/SuccessMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SuccessMenu : MonoBehaviour
+{
+    /**
+     * Starts the game over.
+     */
+    public void RestartGame()
+    {
+        // Unlock the cursor and hide it again
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        SceneManager.UnloadSceneAsync("SuccessMenu");
+        SceneManager.LoadScene("Game");
+    }
+
+    /**
+     * Exits the game.
+     */
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
